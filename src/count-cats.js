c37b9ad814045d464f9829903b59f2371f879479
Tedzury <asdrogachev@gmail.com>
2023-04-15 12:52:33 +0300

    done first two tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  
+  return matrix.flat().reduce((counter, item) =>  item === '^^' ? counter + 1 : counter, 0)
 }
 
+ console.log((countCats([[0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2]])))
 module.exports = {
   countCats
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(arr) {
+
+  if (!Array.isArray(arr)) return false
+
+ return arr.filter(name => typeof name === 'string')
+           .map(name => name.trim())
+           .map(name => name.slice(0, 1)
+           .toUpperCase())
+           .sort((a, b) => a.localeCompare(b)).join('')
+
 }
+
+createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max'])
+createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null])
 
 module.exports = {
   createDreamTeam
