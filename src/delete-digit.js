3b161ae2f701652bcb789ea269bf4d4334ddbe42
Tedzury <asdrogachev@gmail.com>
2023-04-18 00:52:11 +0300

    add delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -12,22 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function deleteDigit(n) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-  
-  // const number = String(n).split('')
-  // const results = []
 
-  // for(let i = 0; i < number.length; i++) {
 
-  //   const arr = [...number];
-  //   arr.splice(i, 1)
-  //   results.push(Number(arr.join('')))
-  // }
+  const number = String(n).split('')
+  const results = []
 
-  // results.sort((a,b) => a - b)
+  for(let i = 0; i < number.length; i++) {
 
-  // return results[results.length - 1]
+    const arr = [...number];
+    arr.splice(i, 1)
+    results.push(Number(arr.join('')))
+  }
+
+  results.sort((a,b) => a - b)
+
+  return results[results.length - 1]
 }
 
 // console.log(deleteDigit(152))
